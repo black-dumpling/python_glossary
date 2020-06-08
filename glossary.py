0d718c8cd6627144731eda9ce785e68e99ad2f07
Arek Podkowa <18arek@gmail.com>
2020-06-08 22:19:30 +0200

    Prettify program messages.

--- glossary.py
+++ glossary.py
@@ -24,16 +24,16 @@ class Glossary:
         term = input("Enter term: ")
         definition = input("Enter definition: ")
         if term in self.glossary[category]:
-            confirm = input(f"\nTerm {term} exists in {category}, do you want"
+            confirm = input(f"\nTerm '{term}' exists in '{category}', do you want"
                   " to  overwrite it? (y/n) ")
             if confirm == 'y':
                 self.glossary[category][term] = definition
-                print(f"\n{term} modified in {category}.")
+                print(f"\n'{term}' modified in '{category}'.")
             else:
                 pass
         else:
             self.glossary[category][term] = definition
-            print(f"\n{term} added to {category}.")
+            print(f"\n'{term}' added to '{category}'.")
 
 
 
@@ -41,7 +41,7 @@ class Glossary:
         """Add another category."""
         category = input("\nEnter a new category: ")
         if category in self.glossary:
-            print(f"{category} already exists and cannot be overwritten!")
+            print(f"'{category}' already exists and cannot be overwritten!")
         elif category not in self.glossary:
             self.glossary[category] = {}
 
@@ -68,17 +68,17 @@ class Glossary:
         """Remove term from the glossary."""
         remove = input("\nEnter term you want to remove: ")
         confirm = input("Are you sure you want to"
-                        f" remove {remove}? (y/n [n]) ")
+                        f" remove '{remove}'? (y/n [n]) ")
         if confirm == 'y':
             for category in self.glossary:
                 if remove in self.glossary[category]:
                     del self.glossary[category][remove]
-                    print(f"{remove} has been removed.")
+                    print(f"\n'{remove}' has been removed from '{category}'.")
                     break
         elif confirm == 'n' or confirm == '':
-            print("Removal canceled.")
+            print("\nRemoval canceled.")
         else:
-            print("Error: use either 'y', 'n' or simply press enter")
+            print("\nError: use either 'y', 'n' or simply press enter")
 
 
     def get_help(self):
